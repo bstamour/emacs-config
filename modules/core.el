000486b5be2631af2ca94efaa2349ed230d8d9ae
Bryan St. Amour <bryan.stamour@gmail.com>
2014-05-15 16:02:38 -0400

    resolve theme.

--- modules/core.el
+++ modules/core.el
@@ -1,8 +1,9 @@
 (require 'cl)
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-resolve)
 
-(if window-system (load-theme 'ample t))
-
-
+;(if window-system (load-theme 'ample t))
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
