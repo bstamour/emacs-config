994c2ceda4408fc7b80aa8c24e9c13bacdedd3e0
Bryan St. Amour <bryan@bryanstamour.com>
2014-12-07 11:29:54 -0500

    Haskell module now works.

--- modules/bst-lang-haskell.el
+++ modules/bst-lang-haskell.el
@@ -14,7 +14,7 @@
   '(progn
      (define-key haskell-mode-map (kbd "C-x C-d") nil)
      (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
      (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
      (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
      (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
