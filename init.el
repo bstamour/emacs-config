2c49a5c57bed57d33dd30dd3094e1286908a0781
Bryan St. Amour <bryan@bryanstamour.com>
2015-11-30 12:32:03 -0500

    root dir stuff

---
+++ init.el
@@ -0,0 +1,26 @@
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(defvar on-windows (string= system-type "windows-nt"))
+
+(require 'package)
+
+(package-initialize)
+(mapc
+ (lambda (repo) (add-to-list 'package-archives repo) t)
+ '(("marmalade" . "http://marmalade-repo.org/packages/")
+   ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(require 'bst-style)
+(if on-windows
+    (require 'bst-windows))
+
+(require 'bst-keybindings)
+(require 'bst-editing)
+
+(require 'bst-org)
+(require 'bst-haskell)
+(require 'bst-common-lisp)
+(require 'bst-cpp)
+(require 'bst-web)
+(require 'bst-git)
+;(require 'bst-r)
