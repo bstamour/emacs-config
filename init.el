2328634a26894f5c942aef8b034c7871b4dbede7
Bryan St. Amour <bryan@bryanstamour.com>
2017-04-12 14:31:58 -0400

    * server/server: updated default themes

--- init.el
+++ init.el
@@ -36,8 +36,8 @@
 ;;;-----------------------------------------------------------------------------
 
 ;; Color theme.
-(defvar *dark-theme* 'solarized-dark)
-(defvar *light-theme* 'solarized-light)
+(defvar *dark-theme* 'tao-yin)
+(defvar *light-theme* 'tao-yang)
 (defvar *current-theme* nil)
 
 (load-theme *dark-theme* t t)
